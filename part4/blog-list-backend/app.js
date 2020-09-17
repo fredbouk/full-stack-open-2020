a2cc70f3a0bbba69e7645a46abb7094a64b68272
Fredrik Boukharta <fredbouk@gmail.com>
2020-09-17 13:48:54 +0200

    4.15

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -3,18 +3,22 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const errorHandler = require('./utils/error-handler')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
-const mongoUrl = config.mongoUrl
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('connected to MongoDB')
   })
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
-app.use(errorHandler)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
