55fba16e945260be772c3230e4549ab15cdcef28
Fredrik Boukharta <fredrik.boukharta@telecomputing.no>
2020-10-12 14:08:23 +0200

    5.7

--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} by {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.title} by {blog.author} <button onClick={toggleVisibility}>hide</button></p>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
