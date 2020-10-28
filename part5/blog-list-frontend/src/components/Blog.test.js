2683a8cfc4faf31f71c803284cea5992aaa51194
Fredrik Boukharta <fredrik.boukharta@telecomputing.no>
2020-10-28 11:49:47 +0100

    5.14

--- part5/blog-list-frontend/src/components/Blog.test.js
+++ part5/blog-list-frontend/src/components/Blog.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog title and author, hides url and number of likes by default', () => {
@@ -23,12 +23,40 @@ test('renders blog title and author, hides url and number of likes by default', 
     <Blog blog={blog} loggedOnUser={loggedOnUser} />
   )
 
-  component.debug()
-
   const compactView = component.container.querySelector('.compactView')
   expect(compactView).toHaveTextContent('React patterns by Michael Chan')
   expect(compactView).not.toHaveAttribute('style', 'display: none;')
 
   const fullView = component.container.querySelector('.fullView')
   expect(fullView).toHaveAttribute('style', 'display: none;')
+})
+
+test('blogs url and number of like are shown when view button is clicked', () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: '7',
+    user: {
+      name: 'Matti Luukkainen'
+    }
+  }
+
+  const loggedOnUser = {
+    name: 'Matti Luukkakinen'
+  }
+
+  const component = render(
+    <Blog blog={blog} loggedOnUser={loggedOnUser} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  component.debug()
+
+  const fullView = component.container.querySelector('.fullView')
+  expect(fullView).toHaveAttribute('style', '')
+  expect(fullView).toHaveTextContent('https://reactpatterns.com/')
+  expect(fullView).toHaveTextContent('7')
 })
