6ddbdcdd0fb3bc0f2493fe41595ede9d180da9dc
Fredrik Boukharta <fredrik.boukharta@telecomputing.no>
2020-10-30 13:09:30 +0100

    5.18

--- part5/blog-list-frontend/cypress/integration/blog_app.spec.js
+++ part5/blog-list-frontend/cypress/integration/blog_app.spec.js
@@ -3,6 +3,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,5 +16,21 @@ describe('Blog app', function () {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
   })
 })
--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -100,6 +100,7 @@ const App = () => {
           <div>
           username
             <input
+              id='username'
               type='text'
               value={username}
               name='Username'
@@ -109,13 +110,14 @@ const App = () => {
           <div>
           password
             <input
+              id='password'
               type='password'
               value={password}
               name='Password'
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type='submit'>login</button>
+          <button id='login-button' type='submit'>login</button>
         </form>
       </div>
     )
